af530690d9b3731b2f0d9d8a2b17e75f289e0007
Ricardo Shiota Yasuda <shadowmaru@users.noreply.github.com>
2019-09-04 19:01:15 -0300

    Verifies if user is active through flags
    Source: https://docs.microsoft.com/en-us/windows/win32/adschema/a-useraccountcontrol

--- app.rb
+++ app.rb
@@ -18,23 +18,40 @@ get '/search' do
 end
 
 def enabled?(code)
-  case code
-  when '512', '544', '66048'
-    true
-  when '514', '66050'
-    false
-  else
-    false
+  return false if flag_list(code).include?('ADS_UF_ACCOUNTDISABLE')
+
+  true
+end
+
+def flag_list(number)
+  hex_number = decimal_to_hex(number)
+
+  array = flags.keys.reverse
+  i = 0
+  count = hex_number
+  flag_list = []
+  while count.positive? do
+    if (count - array[i]) >= 0
+      count -= array[i]
+      flag_list << flags[array[i]]
+    end
+    i += 1
   end
+
+  flag_list
 end
 
 def decimal_to_hex(number)
-  digits = []
-  while number > 0 do
-    digit = number % 16
-    digits << digit
-    number /= 16
-  end
+  number.to_i.to_s(16).to_i
+end
 
-  digits.reverse.join.to_i
+def flags
+  {
+    1 => 'ADS_UF_SCRIPT',
+    2 => 'ADS_UF_ACCOUNTDISABLE',
+    8 => 'ADS_UF_HOMEDIR_REQUIRED',
+    20 => 'ADS_UF_PASSWD_NOTREQD',
+    200 => 'ADS_UF_NORMAL_ACCOUNT',
+    10_000 => 'ADS_UF_DONT_EXPIRE_PASSWD'
+  }
 end
